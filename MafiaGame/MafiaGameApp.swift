a725d07c6b6d41b31514d18ff425f85d09aa2d72
Artem Moiseev <moiseev@physics.msu.ru>
2023-12-01 17:57:12 +0300

    Initial Commit

---
+++ MafiaGame/MafiaGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MafiaGameApp.swift
+//  MafiaGame
+//
+//  Created by Artem on 01.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct MafiaGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
