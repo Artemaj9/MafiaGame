7f7d05091099bb983ee4d500503aa02b4aaa911b
Artem Moiseev <moiseev@physics.msu.ru>
2023-12-01 21:47:57 +0300

    add back and forward buttons

--- MafiaGame/Screens/GameView.swift
+++ MafiaGame/Screens/GameView.swift
@@ -22,29 +22,62 @@ struct GameView: View {
                         .cornerRadius(12, corners: [.bottomLeft, .bottomRight])
                         .padding([.leading, .trailing], 12)
                         .frame(height: geo.size.height/10)
-                    
-                    RoundedRectangle(cornerRadius: 100)
-                        .fill(.white)
-                        .shadow(color: .black.opacity(0.4), radius: 8, x: 0, y: 0)
-                        .frame(width: 70, height: 28)
-                        .offset(y: -24)
-                        .overlay(
-                        Image(systemName: "shevron"))
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 100)
+                            .fill(.white)
+                            .shadow(color: .black.opacity(0.4), radius: 8, x: 0, y: 0)
+                            .frame(width: 70, height: 28)
+
+                            Image(systemName: "chevron.down")
+                            .foregroundColor(.black)
+                            .scaleEffect(1.3)
                         
-                    
-                    Spacer()
-                    
-                    Button {
-                        isDay.toggle()
-                    } label: {
-                        Circle()
-                            .fill(isDay ? .yellow : .blue)
-                            .frame(width: 80)
-                        
-                            .overlay {
-                                Image(isDay ? "sunbtn" : "moonbtn")
-                            }
                     }
+                    .offset(y: -10)
+                        
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    HStack(spacing: 24) {
+                        Button {
+                            
+                        } label: {
+                            Circle()
+                                .fill(.white)
+                                .frame(width: 48)
+                                .shadow(color: isDay ? .black.opacity(0.64) : .white.opacity(0.64), radius: 4)
+                                .overlay {
+                                    Image("back")
+                                }
+                              
+                        }
+                        
+                        Button {
+                            isDay.toggle()
+                        } label: {
+                            Circle()
+                                .fill(isDay ? .yellow : .blue)
+                                .frame(width: 80)
+                                .shadow(color: isDay ? .black.opacity(0.64) : .white.opacity(0.44), radius: 4)
+                            
+                                .overlay {
+                                    Image(isDay ? "sunbtn" : "moonbtn")
+                                }
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            Circle()
+                                .fill(.white)
+                                .frame(width: 48)
+                                .shadow(color: isDay ? .black.opacity(0.64) : .white.opacity(0.64), radius: 4)
+                                .overlay {
+                                    Image("forward")
+                                }
+                        }
+                    }
+                    Spacer()
                 }
                 .ignoresSafeArea()
             }
