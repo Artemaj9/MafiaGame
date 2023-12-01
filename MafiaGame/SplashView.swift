51fd7416ab274c314659b2c2dff09b9c05313d7c
Artem Moiseev <moiseev@physics.msu.ru>
2023-12-01 18:48:59 +0300

    add splashView

---
+++ MafiaGame/SplashView.swift
@@ -0,0 +1,37 @@
+//
+//  SplashView.swift
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        ZStack {
+            Image("bgsplash")
+                .resizable()
+                .ignoresSafeArea()
+            
+            GeometryReader { geo in
+                Image("logoman")
+                    .scaledToFit()
+                    .frame(width: geo.size.width,
+                           height: geo.size.height,
+                           alignment: .center)
+                    .offset(y: -geo.size.height/12)
+                
+                Image("logo")
+                    .frame(width: geo.size.width,
+                           height: geo.size.height,
+                           alignment: .center)
+                    .offset(y: geo.size.height/12)
+            }
+        }
+        
+    }
+}
+
+struct SplashView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
