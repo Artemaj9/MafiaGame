d0e4e00484774d6caba038957648c87036c33d5b
Artem Moiseev <moiseev@physics.msu.ru>
2023-12-01 21:06:30 +0300

    add rounded chevron button background (game view)

--- MafiaGame/Screens/GameView.swift
+++ MafiaGame/Screens/GameView.swift
@@ -10,31 +10,46 @@ struct GameView: View {
     
     var body: some View {
         ZStack {
-            Image(isDay ? "bgday" : "bgnight")
-                .resizable()
-                .ignoresSafeArea()
-                .animation(.easeInOut(duration: 1.5), value: isDay)
-            
-            VStack {
-                Spacer()
-                Button {
-                    isDay.toggle()
-                } label: {
-                    Circle()
-                        .fill(isDay ? .yellow : .blue)
-                        .frame(width: 80)
-                        
-                        .overlay {
-                            Image(isDay ? "sunbtn" : "moonbtn")
-                        }
-                }
-            }
-          //  GeometryReader { geo in
-            //    RoundedRectangle(cornerRadius: 10)
+            GeometryReader { geo in
+                Image(isDay ? "bgday" : "bgnight")
+                    .resizable()
+                    .ignoresSafeArea()
+                    .animation(.easeInOut(duration: 1.5), value: isDay)
                 
-            //}
+                VStack {
+                    Rectangle()
+                        .fill(.white)
+                        .cornerRadius(12, corners: [.bottomLeft, .bottomRight])
+                        .padding([.leading, .trailing], 12)
+                        .frame(height: geo.size.height/10)
+                    
+                    RoundedRectangle(cornerRadius: 100)
+                        .fill(.white)
+                        .shadow(color: .black.opacity(0.4), radius: 8, x: 0, y: 0)
+                        .frame(width: 70, height: 28)
+                        .offset(y: -24)
+                        .overlay(
+                        Image(systemName: "shevron"))
+                        
+                    
+                    Spacer()
+                    
+                    Button {
+                        isDay.toggle()
+                    } label: {
+                        Circle()
+                            .fill(isDay ? .yellow : .blue)
+                            .frame(width: 80)
+                        
+                            .overlay {
+                                Image(isDay ? "sunbtn" : "moonbtn")
+                            }
+                    }
+                }
+                .ignoresSafeArea()
+            }
+            .preferredColorScheme(.light)
         }
-                .preferredColorScheme(.dark)
     }
 }
 
