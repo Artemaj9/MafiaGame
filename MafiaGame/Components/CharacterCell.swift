6bfbe92d4d0d64fac9839cf3c2ce4d0ab7467916
Artem Moiseev <moiseev@physics.msu.ru>
2023-12-03 00:33:15 +0300

    add all characters

--- MafiaGame/Components/CharacterCell.swift
+++ MafiaGame/Components/CharacterCell.swift
@@ -1,8 +1,120 @@
 //
 //  CharacterCell.swift
-//  MafiaGame
-//
-//  Created by Artem on 02.12.2023.
 //
 
-import Foundation
+import SwiftUI
+
+struct CharacterCell: View {
+
+    let character: CharacterModel
+    var myGradient = Gradient(
+        colors: [
+            Color("strokeGrad4"),
+            Color("strokeGrad3"),
+            Color("strokeGrad2")
+        ]
+    )
+    
+    var body: some View {
+        VStack {
+                ZStack {
+                    Color.black.ignoresSafeArea()
+                    GeometryReader { geo in
+                        VStack {
+                            ZStack(alignment: .top) {
+                                Image(character.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .overlay {
+                                        Rectangle()
+                                            .stroke(
+                                                LinearGradient(
+                                                    gradient: myGradient,
+                                                    startPoint: .bottom,
+                                                    endPoint: .top
+                                                ),
+                                                lineWidth: 3
+                                            )
+                                            .cornerRadius(1)
+                                    }
+                                    .padding()
+                                    .background {
+                                        
+                                        ZStack {
+                                            RadialGradient(colors: [Color("citizenBg"),.black], center: .center, startRadius: 80, endRadius: 190)
+                                            Color("citizenBg").opacity(0.4)
+                                            
+                                            RadialGradient(colors: [.black.opacity(0),.black.opacity(0.3) ], center: .center, startRadius: 10, endRadius: 90)
+                                                .mask {
+                                                    Image(character.image + "Rays")
+                                                        .resizable()
+                                                        .scaledToFit()
+                                                    
+                                                }
+                                            
+                                            RadialGradient(colors: [Color("cubeGrad"),Color("cubeGrad").opacity(0.32),Color("cubeGrad").opacity(0) ], center: .center, startRadius: 1, endRadius: 190)
+                                                .blur(radius: 10)
+                                                .offset(y: -24)
+                                                .mask {
+                                                    Image(character.image + "Figure")
+                                                        .resizable()
+                                                        .blur(radius: 2)
+                                                        .scaledToFit()
+                                                }
+                                        }
+                                        .padding()
+                                    }
+                                
+                                Rectangle()
+                                    .fill(
+                                        LinearGradient(gradient: Gradient(colors: [
+                                            Color("magicColor2").opacity(0),
+                                            Color("magicColor2"),
+                                            Color("magicColor2"),
+                                            Color("magicColor2").opacity(0)]),
+                                                       startPoint: .leading,
+                                                       endPoint: .trailing)
+                                    )
+                                    .frame(width: 320, height: 33)
+                                    .overlay(
+                                        Text(character.role)
+                                            .font(Font.custom("Roboto-Black", size: 27))
+                                            .foregroundColor(.white)
+                                            .shadow(color: .black, radius: 4)
+                                    )
+                                    .offset(y: 0.75 * geo.size.width)
+                                HStack {
+                                    Spacer()
+                                    Rectangle()
+                                        .fill(.white)
+                                        .frame(width: 40, height: 33)
+                                        .overlay(
+                                            Text(character.number)
+                                                .font(Font.custom("Roboto-Bold", size: 24)))
+                                        .foregroundColor(.black)
+                                }
+                                .padding(28)
+                            }
+                            
+                            Text(character.description)
+                                .multilineTextAlignment(.center)
+                                .font(Font.custom("Roboto-Medium", size: 18))
+                                .foregroundColor(.white)
+                                .shadow(color: .black, radius: 6)
+                                .padding([.horizontal,.bottom], 16)
+                        }
+                        .background {
+                            Color("magicColor")
+                                .padding(4)
+                        }
+                    }
+                }
+        }
+    }
+}
+
+//struct CharacterCellView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        CharacterCell()
+//    }
+//}
--- MafiaGame/Views/CityView.swift
+++ MafiaGame/Views/CityView.swift
@@ -12,6 +12,33 @@ struct CityView: View {
     "Ordinary townspeople who don't have special abilities. Their main goal is to survive and help identify the Mafia for lynching during the day."),
         CharacterModel(image: "doctor", number: "2", role: "DOCTOR", description:
     "Can protect other players from being eliminated by the Mafia at night. Typically they cannot protect the same person two nights in a row."),
+        CharacterModel(image: "sheriff", number: "3", role: "SHERIFF", description:
+    "A variant of the Detective. They can investigate other players to find out if they are suspicious (Mafia or Serial Killer) or not."),
+        CharacterModel(image: "cop", number: "4", role: "COP", description:
+    "Can investigate other players to determine their roles. They play a crucial role in guiding the citizens to make the right decisions."),
+        CharacterModel(image: "mafia", number: "5", role: "MAFIA", description:
+    "The main antagonists who secretly eliminate citizens at night. They know each other's identity and conspire to deceive the town during the day."),
+        CharacterModel(image: "godfather", number: "6", role: "GODFATHER", description:
+    "The leader of the Mafia. Appears as a Citizen when investigated by the Detective. Immune to the Vigilante's attack."),
+        CharacterModel(image: "mafioso", number: "7", role: "MAFIOSO", description:
+    "A member of the Mafia who carries out the killing. If no Mafioso is present, the Godfather will carry out the kill."),
+        CharacterModel(image: "framer", number: "8", role: "FRAMER", description:
+    "A Mafia role that can frame other players, causing them to appear as Mafia when investigated by the Detective."),
+        CharacterModel(image: "bodyguard", number: "9", role: "BODYGUARD", description:
+    "Can protect a player from attacks at night. If their protected player is attacked, the Bodyguard and the attacker die instead."),
+        CharacterModel(image: "executioner", number: "10", role: "EXECUTIONER", description:
+    "Their goal is to get a specific person lynched. If that person dies at night or survives the game, the Executioner becomes a Jester."),
+        CharacterModel(image: "mistress", number: "11", role: "MISTRESS", description:
+    "Can distract a player each night, blocking them from using their ability."),
+        CharacterModel(image: "jester", number: "12", role: "JESTER", description:
+    "Their goal is to trick the town into lynching them. If they are killed at night or survive the game, they lose."),
+        CharacterModel(image: "vigilante", number: "13", role: "VIGILANTE", description:
+    "A citizen who can choose to kill other players at night. Their ability is usually limited to a certain number of uses. They need to use this power wisely to not accidentally eliminate innocent citizens."),
+        CharacterModel(image: "skiller", number: "14", role: "SERIAL KILLER", description:
+    "An independent role that tries to be the last one standing by killing one player every night. Not aligned with either the Mafia or the Citizens."),
+        
+        
+        
         
                                   ]
     var body: some View {
@@ -27,7 +54,7 @@ struct CityView: View {
                 VStack {
                     ForEach(characters, id: \.self) { character in
                         CharacterCell(character: character)
-                            .frame(height: 500)
+                            .frame(idealHeight: 500, maxHeight: 550)
                     }
                 }
             
