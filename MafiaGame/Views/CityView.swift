af3529371430930673ca5f872278aff4a40b2883
Artem Moiseev <moiseev@physics.msu.ru>
2023-12-02 03:07:17 +0300

    add number and title

--- MafiaGame/Views/CityView.swift
+++ MafiaGame/Views/CityView.swift
@@ -12,40 +12,70 @@ struct CityView: View {
         ZStack {
             Color.black.ignoresSafeArea()
             Color("magicColor")
-                    ZStack {
-                        Color("citizenBg")
-                        RadialGradient(colors: [.black.opacity(0),.black.opacity(0.3) ], center: .center, startRadius: 10, endRadius: 30)
-                            .blur(radius: 10)
-                            .mask {
-                                Image("citizenRays")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .hueRotation(.degrees(75))
-                                    .opacity(1)
+            GeometryReader { geo in
+                ZStack {
+                    Image("citizen")
+                        .resizable()
+                        .scaledToFit()
+                        .padding()
+                        .background {
+                            
+                            ZStack {
+                                RadialGradient(colors: [Color("citizenBg"),.black], center: .center, startRadius: 80, endRadius: 190)
+                                Color("citizenBg").opacity(0.4)
                                 
+                                RadialGradient(colors: [.black.opacity(0),.black.opacity(0.3) ], center: .center, startRadius: 10, endRadius: 90)
+                                    .mask {
+                                        Image("citizenRays")
+                                            .resizable()
+                                            .scaledToFit()
+                                        
+                                    }
+                                
+                                RadialGradient(colors: [Color("cubeGrad"),Color("cubeGrad").opacity(0.32),Color("cubeGrad").opacity(0) ], center: .center, startRadius: 1, endRadius: 190)
+                                    .blur(radius: 10)
+                                    .offset(y: -24)
+                                    .mask {
+                                        Image("citizenFigure")
+                                            .resizable()
+                                            .blur(radius: 2)
+                                            .scaledToFit()
+                                    }
                             }
-                        
-                       
-                    }
-            Image("citizen")
-                .resizable()
-                .scaledToFit()
-                .padding()
-                .background {
-                    RadialGradient(colors: [Color("cubeGrad"),Color("cubeGrad").opacity(0.42),Color("cubeGrad").opacity(0) ], center: .center, startRadius: 1, endRadius: 230)
-                        .offset(y: 0)
-                        .mask {
-                            Image("citizenFigure")
-                                .resizable()
-                                .blur(radius: 2)
-                                .scaledToFit()
+                            .padding()
                         }
+                        
+                    
+                    Rectangle()
+                        .fill(
+                            LinearGradient(gradient: Gradient(colors: [Color("magicColor2").opacity(0), Color("magicColor2"), Color("magicColor2"),Color("magicColor2").opacity(0)]),
+                                                  startPoint: .leading,
+                                                  endPoint: .trailing)
+                          
+                        )
+                        .frame(width: 320, height: 33)
+                        .overlay(
+                            Text("CITIZEN")
+                                .font(Font.custom("Roboto-Black", size: 27))
+                                .foregroundColor(.white)
+                                .shadow(color: .black, radius: 4)
+                        )
+                        .offset(y: geo.size.height/6)  // придумать универсальный сдвиг
+                  
+                            Rectangle()
+                                .fill(.white)
+                                .frame(width: 40, height: 33, alignment: .topTrailing)
+                                .overlay(
+                                    Text("1")
+                                        .font(Font.custom("Roboto-Bold", size: 24)))
+                    }
+                       
+                        
+                    
                 }
-            
-                }
-               
             }
-    }
+        }
+}
 
 struct CityView_Previews: PreviewProvider {
     static var previews: some View {
